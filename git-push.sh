ab9a6391649abcec7096e64628c48a1a43de0b66
Your Name <you@example.com>
2023-08-18 14:53:22 +0200

    ci

--- git-push.sh
+++ git-push.sh
@@ -5,6 +5,7 @@ cd gitops
 mkdir -p production
 cp ../k8s/dist/* ./production
 git add -u .
+git ls-files --deleted -z | xargs -0 git rm
 git commit -m "gitops-$(date +%s )"
 git push --set-upstream origin master
 cd ../
