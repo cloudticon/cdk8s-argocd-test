72238d32e59dc0acdb2c06528c739f6abf084b58
Your Name <you@example.com>
2023-08-18 14:54:02 +0200

    ci

--- git-push.sh
+++ git-push.sh
@@ -3,9 +3,9 @@ git config --global user.name "gitops"
 git clone https://$GH_TOKEN@github.com/cloudticon/gitops ./gitops
 cd gitops
 mkdir -p production
+rm  ./production/*
 cp ../k8s/dist/* ./production
 git add -u .
-git ls-files --deleted -z | xargs -0 git rm
 git commit -m "gitops-$(date +%s )"
 git push --set-upstream origin master
 cd ../
